d3eae2691f9b89a7d976a2375e57904e5f0fbb6a
ismezy <ismezy@qq.com>
2022-04-29 11:40:35 +0800

    feat: swagger support

---
+++ mylib-swagger-web/src/main/java/com/zy/mylib/swagger/web/SwaggerConfig.java
@@ -0,0 +1,78 @@
+package com.zy.mylib.swagger.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+
+/**
+ * @author zhouyang
+ */
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(apiInfo())
+                .securitySchemes(singletonList(apiKey()))
+                .securityContexts(singletonList(securityContext()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("token", "token", "header");
+    }
+
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/.*"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return singletonList(
+                new SecurityReference("token", authorizationScopes));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+    }
+
+    /**
+     * @return an instance of ApiInfo
+     */
+    private ApiInfo apiInfo() {
+
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+
+        return apiInfoBuilder.build();
+    }
+
+}
