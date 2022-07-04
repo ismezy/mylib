4d6a07aaacd5956d7226cc9b2d13c654236d23be
ismezy <ismezy@qq.com>
2022-07-04 15:04:21 +0800

    fix: java 2 kt

--- mylib-swagger-web/src/main/java/com/zy/mylib/swagger/web/SwaggerConfig.kt
+++ mylib-swagger-web/src/main/java/com/zy/mylib/swagger/web/SwaggerConfig.kt
@@ -13,81 +13,68 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.zy.mylib.swagger.web;
+package com.zy.mylib.swagger.web
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.List;
-
-import static java.util.Collections.singletonList;
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.*
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
+import springfox.documentation.spring.web.plugins.Docket
 
 /**
  * @author zhouyang
  */
 @Configuration
-public class SwaggerConfig implements WebMvcConfigurer {
+open class SwaggerConfig : WebMvcConfigurer {
+  @Bean
+  open fun api(): Docket {
+    return Docket(DocumentationType.SWAGGER_2)
+      .select()
+      .apis(RequestHandlerSelectors.any())
+      .paths(PathSelectors.any())
+      .build()
+      .pathMapping("/")
+      .apiInfo(apiInfo())
+      .securitySchemes(listOf<SecurityScheme>(apiKey()))
+      .securityContexts(listOf(securityContext()))
+  }
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/")
-                .apiInfo(apiInfo())
-                .securitySchemes(singletonList(apiKey()))
-                .securityContexts(singletonList(securityContext()));
-    }
+  private fun apiKey(): ApiKey {
+    return ApiKey("token", "token", "header")
+  }
 
-    private ApiKey apiKey() {
-        return new ApiKey("token", "token", "header");
-    }
+  private fun securityContext(): SecurityContext {
+    return SecurityContext.builder()
+      .securityReferences(defaultAuth())
+      .forPaths(PathSelectors.regex("/.*"))
+      .build()
+  }
 
+  private fun defaultAuth(): List<SecurityReference> {
+    val authorizationScope = AuthorizationScope("global", "accessEverything")
+    val authorizationScopes = arrayOfNulls<AuthorizationScope>(1)
+    authorizationScopes[0] = authorizationScope
+    return listOf(
+      SecurityReference("token", authorizationScopes)
+    )
+  }
 
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .forPaths(PathSelectors.regex("/.*"))
-                .build();
-    }
+  override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+    registry.addResourceHandler("/swagger-ui/**")
+      .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+  }
 
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope
-                = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return singletonList(
-                new SecurityReference("token", authorizationScopes));
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
-    }
-
-    /**
-     * @return an instance of ApiInfo
-     */
-    private ApiInfo apiInfo() {
-
-        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-
-        return apiInfoBuilder.build();
-    }
-
+  /**
+   * @return an instance of ApiInfo
+   */
+  private fun apiInfo(): ApiInfo {
+    val apiInfoBuilder = ApiInfoBuilder()
+    return apiInfoBuilder.build()
+  }
 }
